0b07dbef3e3eb9dbaadfef76eeca3924713653f4
Linus <liljalinus@gmail.com>
2017-11-14 19:04:01 +0100

    v-model mjeh

--- src/js/app/components/room/search.vue
+++ src/js/app/components/room/search.vue
@@ -5,12 +5,18 @@
       type="text"
       v-model="searchInput"
       placeholder="Search for track or artist" />
+
+    {{ searchInput }}
   </div>
 
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        searchInput: ''
+      }
+    }
   }
 </script>
